22356c571c5ae0c4fa2765f758dd0d3da46b64fe
Nickitiki <46298023+Nickitiki@users.noreply.github.com>
2019-11-10 17:18:32 +0100

    Added floating action button with onClicklistener

--- app/src/main/java/android/nte/ch/daycontroller/TodoList.java
+++ app/src/main/java/android/nte/ch/daycontroller/TodoList.java
@@ -8,14 +8,16 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.util.Log;
 import android.view.View;
-import android.widget.TableLayout;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
 public class TodoList extends AppCompatActivity {
 
     private static final String TAG = "TodoList";
     private SectionsPagerAdapter sectionsPagerAdapter;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +29,14 @@ public class TodoList extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(R.string.todos);
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.todo_view_pager);
+        ViewPager viewPager = findViewById(R.id.todo_view_pager);
         setUpViewPager(viewPager);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.todo_tabs);
+        TabLayout tabLayout = findViewById(R.id.todo_tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        fab = findViewById(R.id.todo_fab);
+        fabOnClickListener();
     }
 
     private void setUpViewPager(ViewPager viewPager){
@@ -39,5 +44,14 @@ public class TodoList extends AppCompatActivity {
         adapter.addFragment(new TodoActualTab(), getResources().getString(R.string.actual));
         adapter.addFragment(new TodoDoneTab(), getResources().getString(R.string.done));
         viewPager.setAdapter(adapter);
+    }
+
+    private void fabOnClickListener(){
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(TodoList.this, "test", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
